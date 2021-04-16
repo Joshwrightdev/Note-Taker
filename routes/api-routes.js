05c79762ed2ae77ded87a7948df5bdffc0a2b9f6
Josh Wright <joshwrightaudio@gmail.com>
2021-04-16 08:30:07 -0400

    rework code for api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,19 @@
+const db = require("../db/db.json");
 const fs = require("fs");
 
-
-
-
-
-
-function modifyNotes() {
-  fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
-    if (err) throw err;
-    return true;
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(db);
   });
-}
+  app.delete("/api/notes/:id", function (req, res) {
+    var id = req.params.id;
+    db.splice(id - 1, 1);
+    db.forEach((obj, i) => {
+      obj.id = i + 1;
+    });
 
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
+  });
+};
