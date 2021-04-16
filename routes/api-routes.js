f63d6e2c0f33f00c73f6c5ee2551f6f357245587
Josh Wright <joshwrightaudio@gmail.com>
2021-04-16 09:45:55 -0400

    working code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,4 +16,15 @@ module.exports = function (app) {
       res.json(db);
     });
   });
+
+  app.post("/api/notes", function (req, res) {
+    db.push(req.body);
+    db.forEach((obj, i) => {
+      obj.id = i + 1;
+    });
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
+  });
+  
 };
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static);
-require 
+app.use(express.static(__dirname + "/public"));
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
